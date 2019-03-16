1c7f9665bb78257cb387c8e6fc49226464a5ff44
widohupuhe <48523698+widohupuhe@users.noreply.github.com>
2019-03-16 10:34:08 +0300

    Update and rename eeeeeee.js to rr.js

--- rr.js
+++ rr.js
@@ -20,7 +20,11 @@ setInterval(function () {
 		$('#vidrek').remove();
 		$( '#plx').each(function () {
     			this.style.setProperty( 'display', 'block', 'important' ); });
+		$( '.orta').each(function () {
+    			this.style.setProperty( 'margin', '16px auto 0', 'important' ); });
 	});
 
 }, 5);
+
+argin: 180px auto 0!important;
 
