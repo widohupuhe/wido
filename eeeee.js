d8b5ecd5915bc74f8bd13b3401969a81e22401e5
widohupuhe <48523698+widohupuhe@users.noreply.github.com>
2019-03-16 10:26:16 +0300

    Update and rename eeee.js to eeeee.js

---
+++ eeeee.js
@@ -0,0 +1,24 @@
+'use strict';
+
+setInterval(function () {
+
+	$(document).ready(function () {
+		
+		$('#DEfVqHOg').remove();
+		$('#plust').remove();
+		$('#plaltrk').remove();
+		
+		$('.r985x').remove();
+		$('.reklam-728-90').remove();
+		$('.rekgec-div').remove();
+		$('.rk300x').remove();
+		$('.fbbegen').remove();
+		$('.film-yorumlari-orta .sosyal').remove();
+		
+		$('#vidrek').remove();
+		$( '#plx').each(function () {
+    			this.style.setProperty( 'display', 'block', 'important' ); });
+	});
+
+}, 5);
+
