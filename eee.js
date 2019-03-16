581494a99b534de4a915acf084fe2aa99bb508df
widohupuhe <48523698+widohupuhe@users.noreply.github.com>
2019-03-16 09:59:08 +0300

    Update and rename ee.js to eee.js

--- eee.js
+++ eee.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.src = 'http://vibeshtml.dharansh.in/assets/vendors/jquery.min.js';
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js';
 document.getElementsByTagName('body')[0].appendChild(script);
 
 
